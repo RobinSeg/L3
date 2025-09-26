a357efdd3526c1a0e82d8f51719c37db8988767f
Yugokan <robin.sgd59@gmail.com>
2025-09-26 15:39:34 +0200

    Add files via upload

--- Web/TD2_JS/script/formulaire.js
+++ Web/TD2_JS/script/formulaire.js
@@ -24,9 +24,35 @@ console.log("Max volume :", vol.max);
 vol.oninput = function() {
 document.getElementById("valeur").textContent = this.value;
 };
+document.getElementById("valeur").textContent = vol.value;
 
+
+document.querySelector("label[for='coche']").textContent = "Mute";
+
+document.getElementById("coche").onchange = function() {
+  vol.disabled = this.checked;
+  document.getElementById("valeur").textContent = 0;
+};
 
 let img = document.createElement("img");
 img.src = "https://upload.wikimedia.org/wikipedia/commons/b/bd/UPHF_logo.svg";
 img.width = 200;
 document.getElementById("lien").appendChild(img);
+
+
+
+const prog1 = document.getElementById("prog1");
+const prog2 = document.getElementById("prog2");
+
+prog2.value = 45;
+
+let val = 0;
+let timer = setInterval(() => {
+  if (val <= 100) {
+    prog1.value = val;
+    val += 5;
+  } 
+  else {
+    val = 0;
+  }
+}, 400);
